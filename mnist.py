00c8dc3ac9b908a16bc479e7e2921bfc5742c936
Nathan <Nathan_duran@hotmail.com>
2018-07-19 16:14:09 +0100

    Finish Initial Network

--- mnist.py
+++ mnist.py
@@ -1,56 +1,108 @@
+import os
 import gzip
 import pickle
 import utilities as utils
 import numpy as np
 import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow debugging
+########
+# https://www.oreilly.com/learning/not-another-mnist-tutorial-with-tensorflow
 
 data_path = 'data/'
 
 # Load training and test data
 with gzip.open(data_path + 'mnist.pkl', 'rb') as file:
-    train_x = pickle.load(file)
-    train_y = pickle.load(file)
-    test_x = pickle.load(file)
-    test_y = pickle.load(file)
+    dataset = pickle.load(file)
 
-num_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 500
+train_images = dataset['train_images']
+train_labels = dataset['train_labels']
+test_images = dataset['test_images']
+test_labels = dataset['test_labels']
+
+input_dim = 784  # 28 x 28
+num_hidden_nodes = 500
 num_classes = 10
 batch_size = 100
+num_epochs = 2
+learning_rate = 0.01
 
-print(train_x.shape)
-print(train_y.shape)
-print(test_x.shape)
-print(test_y.shape)
-utils.display_digit(train_x, train_y, 1)
+print(train_images.shape)
+print(train_labels.shape)
+print(test_images.shape)
+print(test_labels.shape)
 
-x = tf.placeholder(tf.float32, [None, 784])
-y = tf.placeholder(tf.float32)
+# Display a random digit
+# utils.display_digit(train_x, train_y, np.random.randint(0, train_x.shape[0]))
 
-# def neural_network_model(input_data):
-#
-#     hidden_layer_1 = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-#                       'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
-#
-#     hidden_layer_2 = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-#                       'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
-#
-#     hidden_layer_3 = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-#                       'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
-#
-#     output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-#                     'biases': tf.Variable(tf.random_normal([n_classes]))}
-#
-#     l1 = tf.add(tf.matmul(input_data, hidden_layer_1['weights']), hidden_layer_1['biases'])
-#     l1 = tf.nn.relu(l1)
-#
-#     l2 = tf.add(tf.matmul(l1, hidden_layer_2['weights']), hidden_layer_2['biases'])
-#     l2 = tf.nn.relu(l2)
-#
-#     l3 = tf.add(tf.matmul(l2, hidden_layer_3['weights']), hidden_layer_3['biases'])
-#     l3 = tf.nn.relu(l3)
-#
-#     output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
-#
-#     return output
+x = tf.placeholder(tf.float32, [None, input_dim])
+y = tf.placeholder(tf.int64)
+
+
+def neural_network_model(input_data):
+
+    hidden_layer_1 = {'weights': tf.Variable(tf.random_normal([input_dim, num_hidden_nodes])),
+                      'biases': tf.Variable(tf.random_normal([num_hidden_nodes]))}
+
+    hidden_layer_2 = {'weights': tf.Variable(tf.random_normal([num_hidden_nodes, num_hidden_nodes])),
+                      'biases': tf.Variable(tf.random_normal([num_hidden_nodes]))}
+
+    hidden_layer_3 = {'weights': tf.Variable(tf.random_normal([num_hidden_nodes, num_hidden_nodes])),
+                      'biases': tf.Variable(tf.random_normal([num_hidden_nodes]))}
+
+    output_layer = {'weights': tf.Variable(tf.random_normal([num_hidden_nodes, num_classes])),
+                    'biases': tf.Variable(tf.random_normal([num_classes]))}
+
+    l1 = tf.add(tf.matmul(input_data, hidden_layer_1['weights']), hidden_layer_1['biases'])
+    l1 = tf.nn.relu(l1)
+
+    l2 = tf.add(tf.matmul(l1, hidden_layer_2['weights']), hidden_layer_2['biases'])
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.add(tf.matmul(l2, hidden_layer_3['weights']), hidden_layer_3['biases'])
+    l3 = tf.nn.relu(l3)
+
+    output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
+
+    return output
+
+
+def train_neural_network(input_data):
+
+    # Pass data through the model
+    prediction = neural_network_model(input_data)
+
+    # Calculate the cost
+    # cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction, labels=y) ) / train_x.shape[0]
+    cost = tf.reduce_mean(tf.losses.sparse_softmax_cross_entropy(y, prediction))
+
+    # Minimise the cost with optimisation function
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    with tf.Session() as sess:
+        # Initialise all the variables
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(num_epochs):
+            epoch_loss = 0
+            # for _ in range(int(mnist.train.num_examples / batch_size)):
+            #     epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+            _, c = sess.run([optimizer, cost], feed_dict={x: train_images, y: train_labels})
+
+            epoch_loss = c
+
+            print("Epoch", epoch + 1, "completed out of", num_epochs, "loss:", epoch_loss)
+
+        # Count how many predictions (index of highest probability) match the labels
+        correct = tf.equal(tf.argmax(prediction, axis=1), y)
+
+        # Count how many are correct
+        num_correct = tf.reduce_sum(tf.cast(correct, tf.int32))
+
+        # Calculate the average for correct predictions
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+        print("Number Correct: " + str(num_correct.eval(feed_dict={x: test_images, y: test_labels})) + " / " + str(test_images.shape[0]))
+        print("Accuracy: " + str(accuracy.eval(feed_dict={x: test_images, y: test_labels})))
+
+
+train_neural_network(x)
--- process_mnist.py
+++ process_mnist.py
@@ -5,10 +5,10 @@ import numpy as np
 
 data_path = 'data/'
 
-train_x = []
-train_y = []
-test_x = []
-test_y = []
+train_images = []
+train_labels = []
+test_images = []
+test_labels = []
 
 # Unzip csv file
 with gzip.open(data_path + 'mnist_train.csv.gz', 'rt') as file:
@@ -16,35 +16,37 @@ with gzip.open(data_path + 'mnist_train.csv.gz', 'rt') as file:
 
     for line in csv_data:
         # First column are the labels
-        train_y.append(line[0])
+        train_labels.append(line[0])
         # Remaining columns are the data
-        train_x.append(line[1:])
+        train_images.append(line[1:])
 
 with gzip.open(data_path + 'mnist_test.csv.gz', 'rt') as file:
     csv_data = csv.reader(file, delimiter=',')
 
     for line in csv_data:
         # First column are the labels
-        test_y.append(line[0])
+        test_labels.append(line[0])
         # Remaining columns are the data
-        test_x.append(line[1:])
+        test_images.append(line[1:])
 
 # Convert to numpy arrays
-train_y = np.array(train_y, dtype='int')
-train_x = np.array(train_x, dtype='int')
-test_x = np.array(test_x, dtype='int')
-test_y = np.array(test_y, dtype='int')
+train_images = np.array(train_images, dtype='int')
+train_labels = np.array(train_labels, dtype='int')
+test_images = np.array(test_images, dtype='int')
+test_labels = np.array(test_labels, dtype='int')
 
 # Calculate number of classes
-num_classes = len(np.unique(train_y))
-
-# Convert labels to one-hot encodings
-train_y = np.eye(num_classes, dtype='int')[train_y]
-test_y = np.eye(num_classes, dtype='int')[test_y]
+# num_classes = len(np.unique(train_y))
+#
+# # Convert labels to one-hot encodings
+# train_y = np.eye(num_classes, dtype='int')[train_y]
+# test_y = np.eye(num_classes, dtype='int')[test_y]
 
 # Pickle
+data = {'train_images': train_images, 'train_labels': train_labels, 'test_images': test_images, 'test_labels': test_labels}
 with gzip.open(data_path + 'mnist.pkl', 'wb') as file:
-    pickle.dump(train_x, file)
-    pickle.dump(train_y, file)
-    pickle.dump(test_x, file)
-    pickle.dump(test_y, file)
+    pickle.dump(data, file)
+    # pickle.dump(train_images, file)
+    # pickle.dump(train_labels, file)
+    # pickle.dump(test_images, file)
+    # pickle.dump(test_labels, file)
