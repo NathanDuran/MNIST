544814657314800f9f0ca99ffb712b5b2cea3585
Nathan <nathan_duran@hotmail.com>
2019-07-05 02:34:09 +0100

    Fix Batch Metrics for Tensorboard

--- mnist_multi_layer.py
+++ mnist_multi_layer.py
@@ -1,6 +1,7 @@
 import os
 import time
 import datetime
+import numpy as np
 import tensorflow as tf
 from tensorflow_utilities import nn_layer, dataset_placeholder
 from mnist_utilities import display_weights_matrix_large, process_mnist
@@ -45,24 +46,22 @@ print("------------------------------------")
 print("Define Graph...")
 
 # Define input dataset
-iterator, images_placeholder, labels_placeholder = dataset_placeholder(input_size, num_classes, batch_size,
-                                                                       'input_dataset')
+iterator, images_placeholder, labels_placeholder = dataset_placeholder(input_size, num_classes, batch_size, 'input_dataset')
 images, labels = iterator.get_next()
 
 # Define the network
-hidden_1, hidden_w1 = nn_layer(images, input_size, num_hidden_nodes, activation_func=tf.nn.relu,
-                               layer_name='hidden_layer_1')
-hidden_2, hidden_w2 = nn_layer(hidden_1, num_hidden_nodes, num_hidden_nodes, activation_func=tf.nn.relu,
-                               layer_name='hidden_layer_2')
-hidden_3, hidden_w3 = nn_layer(hidden_2, num_hidden_nodes, num_hidden_nodes, activation_func=tf.nn.relu,
-                               layer_name='hidden_layer_3')
-prediction, _ = nn_layer(hidden_3, num_hidden_nodes, num_classes, activation_func=tf.identity,
-                         layer_name='output_layer')
+hidden_1, hidden_w1 = nn_layer(images, input_size, num_hidden_nodes,
+                               activation_func=tf.nn.relu, layer_name='hidden_layer_1')
+hidden_2, hidden_w2 = nn_layer(hidden_1, num_hidden_nodes, num_hidden_nodes,
+                               activation_func=tf.nn.relu, layer_name='hidden_layer_2')
+hidden_3, hidden_w3 = nn_layer(hidden_2, num_hidden_nodes, num_hidden_nodes,
+                               activation_func=tf.nn.relu, layer_name='hidden_layer_3')
+prediction, _ = nn_layer(hidden_3, num_hidden_nodes, num_classes,
+                         activation_func=tf.identity, layer_name='output_layer')
 
 # Calculate the cost
 with tf.name_scope('cross_entropy'):
     loss = tf.losses.softmax_cross_entropy(labels, logits=prediction)
-    loss_summary = tf.summary.scalar('Loss', loss)
 
 # Minimise the cost with optimisation function
 with tf.name_scope('optimizer'):
@@ -72,7 +71,6 @@ with tf.name_scope('optimizer'):
 with tf.name_scope('accuracy'):
     correct_prediction = tf.equal(tf.argmax(prediction, axis=1), tf.argmax(labels, axis=1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    accuracy_summary = tf.summary.scalar('Accuracy', accuracy)
 
 # Create images for each nodes weights
 with tf.name_scope('tb_images'):
@@ -88,7 +86,6 @@ with tf.Session() as sess:
     # Create Tensorboard writers for the training and test data
     train_writer = tf.summary.FileWriter('%s/%s' % (tensorboard_path, 'train'), sess.graph)
     test_writer = tf.summary.FileWriter('%s/%s' % (tensorboard_path, 'test'), sess.graph)
-    summary = tf.summary.merge([accuracy_summary, loss_summary])
 
     # Initialise all the variables
     sess.run(tf.global_variables_initializer())
@@ -108,19 +105,28 @@ with tf.Session() as sess:
         # Loop over each training batch once per epoch
         batch_accuracy = 0
         batch_loss = 0
-        train_accuracy = []
-        train_loss = []
-        train_summary = None
+        epoch_accuracies = []
+        epoch_losses = []
         while True:
             try:
-                train_summary, _, batch_loss, batch_accuracy = sess.run([summary, optimizer, loss, accuracy])
-                train_loss.append(batch_loss)
-                train_accuracy.append(batch_accuracy)
+                _, batch_loss, batch_accuracy = sess.run([optimizer, loss, accuracy])
+                epoch_accuracies.append(batch_accuracy)
+                epoch_losses.append(batch_loss)
             except tf.errors.OutOfRangeError:
                 break
 
+        # Calculate the epoch loss and accuracy
+        train_accuracy = np.mean(epoch_accuracies)
+        train_loss = np.mean(epoch_losses)
+
         # Record training summaries
-        train_writer.add_summary(train_summary, epoch)
+        accuracy_summary = tf.Summary()
+        accuracy_summary.value.add(tag="Accuracy", simple_value=train_accuracy)
+        train_writer.add_summary(accuracy_summary, epoch)
+
+        loss_summary = tf.Summary()
+        loss_summary.value.add(tag="Loss", simple_value=train_loss)
+        train_writer.add_summary(loss_summary, epoch)
 
         # Initialise the iterator with the test data
         sess.run(iterator.initializer, feed_dict={images_placeholder: test_images, labels_placeholder: test_labels})
@@ -128,34 +134,41 @@ with tf.Session() as sess:
         # Loop over each test batch once per epoch
         batch_accuracy = 0
         batch_loss = 0
-        test_accuracy = []
-        test_loss = []
-        test_summary = None
+        epoch_accuracies = []
+        epoch_losses = []
         while True:
             try:
-                test_summary, batch_loss, batch_accuracy = sess.run([summary, loss, accuracy])
-                test_loss.append(batch_loss)
-                test_accuracy.append(batch_accuracy)
+                batch_loss, batch_accuracy = sess.run([loss, accuracy])
+                epoch_accuracies.append(batch_accuracy)
+                epoch_losses.append(batch_loss)
             except tf.errors.OutOfRangeError:
                 break
 
+        # Calculate the epoch loss and accuracy
+        test_accuracy = np.mean(epoch_accuracies)
+        test_loss = np.mean(epoch_losses)
+
         # Record test and image summaries
-        test_writer.add_summary(test_summary, epoch)
+        accuracy_summary = tf.Summary()
+        accuracy_summary.value.add(tag="Accuracy", simple_value=test_accuracy)
+        test_writer.add_summary(accuracy_summary, epoch)
+
+        loss_summary = tf.Summary()
+        loss_summary.value.add(tag="Loss", simple_value=test_loss)
+        test_writer.add_summary(loss_summary, epoch)
+
         test_writer.add_summary(image_summary.eval(), epoch)
 
         # Display learned weights for first layer
         if epoch < 10 or epoch >= 10 and epoch % 10 == 0:
-            display_weights_matrix_large(hidden_w1, num_hidden_nodes, epoch,
-                                         ((sum(test_accuracy) * 100) / len(test_accuracy)), save=False, path=image_path)
+            display_weights_matrix_large(hidden_w1, num_hidden_nodes, epoch, test_accuracy, save=False, path=image_path)
 
         # Display epoch statistics
         print("Epoch: {}/{} - "
-              "Training loss: {:.3f}, acc: {:.2f}% - "
-              "Test loss: {:.3f}, acc: {:.2f}%".format(epoch, num_epochs,
-                                                       (sum(train_loss) / len(train_loss)),
-                                                       ((sum(train_accuracy) * 100) / len(train_accuracy)),
-                                                       (sum(test_loss) / len(test_loss)),
-                                                       ((sum(test_accuracy) * 100) / len(test_accuracy))))
+              "Training loss: {:.3f}, acc: {:.3f}% - "
+              "Test loss: {:.3f}, acc: {:.3f}%".format(epoch, num_epochs,
+                                                       train_loss, train_accuracy * 100,
+                                                       test_loss, test_accuracy * 100))
 
     end_time = time.time()
     print("Training took " + str(('%.3f' % (end_time - start_time))) + " seconds for", num_epochs, "epochs")
